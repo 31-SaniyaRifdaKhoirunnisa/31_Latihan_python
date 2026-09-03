222a82a6379ae75af6950e1b1ab1d9d05334f8fc
31/Saniya Rifda Khoirunnisa <nisa.tkjsmeksa25@gmail.com>
2026-09-03 14:51:16 +0700

    hitung_geometri

---
+++ hitung_geometri.py
@@ -0,0 +1,11 @@
+def cari_luas_persegi(sisi):
+    return sisi * sisi
+
+def cari_keliling_persegi(sisi):
+    return 4 * sisi
+
+def cari_luas_lingkaran(r):
+    return 3.14 * r * r
+
+def cari_luas_segitiga(a, t):
+    return 0.5 * a * t
